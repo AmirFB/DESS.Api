8a1d66bf247b121615d9dd8e5a04c0e0c7d03a11
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-09-01 17:18:32 +0430

    Async added to related method names.

--- Repositories/IRepositoryBase.cs
+++ Repositories/IRepositoryBase.cs
@@ -6,19 +6,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Dess.Repositories
 {
-    public interface IRepositoryBase<T> : IDisposable
-    {
-        Task<bool> Exists(int id);
-        Task<bool> Exists(T entity);
-        Task<T> GetAsync(int id);
-        Task<IEnumerable<T>> GetAllAsync();
-        Task<IEnumerable<T>> GetAllAsync(IEnumerable<int> ids);
+  public interface IRepositoryBase<T> : IDisposable
+  {
+    Task<bool> ExistsAsync(int id);
+    Task<bool> ExistsAsync(T entity);
+    Task<T> GetAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<IEnumerable<T>> GetAllAsync(IEnumerable<int> ids);
 
-        void Add(T entity);
-        void Update(T entity);
-        void Remove(T entity);
-        Task Remove(int id);
+    void Add(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+    Task RemoveAsync(int id);
 
-        Task<bool> SaveAsync();
-    }
+    Task<bool> SaveAsync();
+  }
 }
--- Repositories/RepositoryBase.cs
+++ Repositories/RepositoryBase.cs
@@ -9,73 +9,73 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Dess.Repositories
 {
-	public class RepositoryBase<TEntity, TContext> : IRepositoryBase<TEntity>
-		where TEntity : EntityBase
-		where TContext : DbContext
-	{
-		protected TContext Context { get; set; }
-		protected DbSet<TEntity> Entities => Context.Set<TEntity>();
+  public class RepositoryBase<TEntity, TContext> : IRepositoryBase<TEntity>
+    where TEntity : EntityBase
+    where TContext : DbContext
+  {
+    protected TContext Context { get; set; }
+    protected DbSet<TEntity> Entities => Context.Set<TEntity>();
 
-		public RepositoryBase(TContext context) =>
-			Context = context ?? throw new ArgumentNullException(nameof(context));
+    public RepositoryBase(TContext context) =>
+      Context = context ?? throw new ArgumentNullException(nameof(context));
 
-		public async Task<bool> Exists(int id) =>
-			await Entities.AnyAsync(e => e.Id == id);
+    public async Task<bool> ExistsAsync(int id) =>
+      await Entities.AnyAsync(e => e.Id == id);
 
-		public async Task<bool> Exists(TEntity entity) =>
-			await Entities.ContainsAsync(entity);
+    public async Task<bool> ExistsAsync(TEntity entity) =>
+      await Entities.ContainsAsync(entity);
 
-		public async Task<TEntity> GetAsync(int id)
-		{
-			if (id <= 0)
-				throw new ArgumentNullException(nameof(id));
+    public async Task<TEntity> GetAsync(int id)
+    {
+      if (id <= 0)
+        throw new ArgumentNullException(nameof(id));
 
-			return await Entities.FindAsync(id);
-		}
+      return await Entities.FindAsync(id);
+    }
 
-		public async Task<IEnumerable<TEntity>> GetAllAsync() =>
-			await Entities.ToListAsync();
+    public async Task<IEnumerable<TEntity>> GetAllAsync() =>
+      await Entities.ToListAsync();
 
-		public async Task<IEnumerable<TEntity>> GetAllAsync(IEnumerable<int> ids) =>
-			await Entities.Where(e => ids.Contains(e.Id)).ToListAsync();
+    public async Task<IEnumerable<TEntity>> GetAllAsync(IEnumerable<int> ids) =>
+      await Entities.Where(e => ids.Contains(e.Id)).ToListAsync();
 
-		public void Add(TEntity entity)
-		{
-			if (entity == null)
-				throw new ArgumentNullException(nameof(entity));
+    public void Add(TEntity entity)
+    {
+      if (entity == null)
+        throw new ArgumentNullException(nameof(entity));
 
-			Entities.Add(entity);
-		}
+      Entities.Add(entity);
+    }
 
-		public void Update(TEntity entity)
-		{
-			if (entity == null)
-				throw new ArgumentNullException(nameof(entity));
+    public void Update(TEntity entity)
+    {
+      if (entity == null)
+        throw new ArgumentNullException(nameof(entity));
 
-			Entities.Update(entity);
-		}
+      Entities.Update(entity);
+    }
 
-		public void Remove(TEntity entity) => Entities.Remove(entity);
-		public async Task Remove(int id) => Remove(await Entities.FindAsync(id));
+    public void Remove(TEntity entity) => Entities.Remove(entity);
+    public async Task RemoveAsync(int id) => Remove(await Entities.FindAsync(id));
 
-		public async Task<bool> SaveAsync() => await Context.SaveChangesAsync() > 0;
+    public async Task<bool> SaveAsync() => await Context.SaveChangesAsync() > 0;
 
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
+    public void Dispose()
+    {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
 
-		protected virtual void Dispose(bool disposing)
-		{
-			if (disposing)
-			{
-				if (Context != null)
-				{
-					Context.Dispose();
-					Context = null;
-				}
-			}
-		}
-	}
+    protected virtual void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        if (Context != null)
+        {
+          Context.Dispose();
+          Context = null;
+        }
+      }
+    }
+  }
 }
