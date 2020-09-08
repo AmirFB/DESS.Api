a9681ea9fa3acc1f8b8edf382a46c45b3f2c241c
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-09-08 20:29:14 +0430

    ElectroFenceHub added to send status to clients on update.

--- Controllers/ElectroFenceHwController.cs
+++ Controllers/ElectroFenceHwController.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Threading.Tasks;
 using AutoMapper;
+using Dess.Hubs;
 using Dess.Models;
 using Dess.Models.ElectroFence;
 using Dess.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Dess.Controllers
 {
@@ -14,24 +16,33 @@ namespace Dess.Controllers
   {
     private readonly IElectroFenceRepository _repository;
     private readonly IMapper _mapper;
+    private readonly IHubContext<ElectroFenceHub> _hubContext;
 
-    public ElectroFenceRepository(IElectroFenceRepository electroFenceRepository, IMapper mapper)
+    public ElectroFenceRepository(IElectroFenceRepository electroFenceRepository, IMapper mapper, IHubContext<ElectroFenceHub> hubContext)
     {
       _repository = electroFenceRepository ??
           throw new ArgumentNullException(nameof(electroFenceRepository));
+
       _mapper = mapper ??
           throw new ArgumentNullException(nameof(mapper));
+
+      _hubContext = hubContext ??
+          throw new ArgumentNullException(nameof(_hubContext));
     }
 
     [HttpPost("{serial}/{configHash}")]
-    public async Task<IActionResult> UpdateAsync(string serial, string configHash, [FromBody] ElectroFenceStatusDto status)
+    public async Task<IActionResult> UpdateAsync(string serial, string configHash, [FromBody] ElectroFenceStatusFromHwDto status)
     {
       var ef = await _repository.GetAsync(serial);
 
       if (ef == null)
         return NotFound();
 
+      var statusDto = _mapper.Map<ElectroFenceDto>(status);
+      await _hubContext.Clients.All.SendAsync("UpdateStatus", statusDto);
+
       _mapper.Map(status, ef.Status);
+      ef.Status.Date = DateTime.Now;
       ef.Applied = configHash == ef.Hash;
 
       var statusHash = (status as IHashable).GetHash();
