e32ae105cafe91e851af6f4dcaec108777c95b55
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-09-08 20:49:58 +0430

    RegisterAsync created.

--- Hubs/ElectroFenceHub.cs
+++ Hubs/ElectroFenceHub.cs
@@ -1,12 +1,24 @@
 using System.Threading.Tasks;
+using Dess.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Dess.Hubs
 {
+  [Authorize]
   public class ElectroFenceHub : Hub
   {
-    public async Task AuthenticateAsync()
+    private readonly IUserRepository _userRepository;
+    public ElectroFenceHub(IUserRepository userRepository) =>
+      userRepository = _userRepository;
+
+    public async Task RegisterAsync(string token)
     {
+      var user = await _userRepository.GetAsync(int.Parse(Context.User.Identity.Name));
+
+      if (user == null)
+        return;
+
 
     }
   }
