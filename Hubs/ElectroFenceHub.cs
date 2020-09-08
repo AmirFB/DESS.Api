00ed1813db58f3d61bad5303928def6494553c08
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-09-08 20:30:43 +0430

    ElectroFenceHub created.

--- Hubs/ElectroFenceHub.cs
+++ Hubs/ElectroFenceHub.cs
@@ -1,9 +1,13 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
 namespace Dess.Hubs
 {
   public class ElectroFenceHub : Hub
   {
+    public async Task AuthenticateAsync()
+    {
 
+    }
   }
 }
