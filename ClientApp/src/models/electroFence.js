1ac4f9658bcd0dd10dccd770ef980755360e4891
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-09-14 20:57:49 +0430

    "ElectroFence", "ElectroFenceStatus" and "User" models added.

---
+++ ClientApp/src/models/electroFence.js
@@ -0,0 +1,28 @@
+export class ElectroFence {
+  id;
+
+  serial;
+  phoneNumber;
+
+  autoLocation;
+  latitude;
+  longitude;
+
+  hvEnabled;
+  lvEnabled;
+
+  hvPower;
+  hvThreshold;
+  hvRepeat;
+
+  temperatureMin;
+  temperatureMax;
+
+  batteryMin;
+  batteryMax;
+
+  Input1;
+  Input2;
+  Output1;
+  Output2;
+}
---
+++ ClientApp/src/models/electroFenceStatus.js
@@ -0,0 +1,23 @@
+export class ElectroFenceStatus {
+  id;
+  applied;
+
+  hvAlarm;
+  lvAlarm;
+  tamperAlarm;
+
+  mainPowerFault;
+  hvPowerFault;
+  hvChargeFault;
+  hvDischargeFault;
+
+  hvVoltage;
+  temperature;
+  batteryStatus;
+  batteryLevel;
+
+  input1;
+  input2;
+  output1;
+  output2;
+}
