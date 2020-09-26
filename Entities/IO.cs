582701b4dc67b33b10a737ff7cc6994b96e0b8f7
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-09-26 13:41:53 +0330

    "Name" property added.

--- Entities/IO.cs
+++ Entities/IO.cs
@@ -6,7 +6,11 @@ namespace Dess.Entities
 {
   public class IO : EntityBase, IHashable
   {
+    public string Name { get; set; }
+
+    [Required]
     public IOType Type { get; set; } = IOType.NO;
+    [Required]
     public bool Enabled { get; set; } = true;
 
     [Required]
---
+++ Models/ElectroFence/IODto.cs
@@ -0,0 +1,13 @@
+﻿using Dess.Types;
+
+namespace Dess.Models.ElectroFence
+{
+  public class IODto
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public IOType Type { get; set; }
+    public bool Enabled { get; set; }
+  }
+}
