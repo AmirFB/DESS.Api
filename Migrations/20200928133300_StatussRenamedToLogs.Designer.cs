1f4631ec33a4525f2e0a148a1f1fe1255b588377
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-09-28 20:12:48 +0330

    "Statuss" renamed to "Logs" migration done.

--- Migrations/20200928133300_StatussRenamedToLogs.Designer.cs
+++ Migrations/20200928133300_StatussRenamedToLogs.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Dess.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DESS.Migrations
 {
     [DbContext(typeof(DessDbContext))]
-    partial class DessDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200928133300_StatussRenamedToLogs")]
+    partial class StatussRenamedToLogs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
