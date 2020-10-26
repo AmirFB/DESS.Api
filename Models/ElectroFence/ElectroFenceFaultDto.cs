beb5c9da6e2dff6bce7dc125006bfe8cac56a5f8
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-10-26 18:10:51 +0330

    Added FaultType.

--- Models/ElectroFence/ElectroFenceFaultDto.cs
+++ Models/ElectroFence/ElectroFenceFaultDto.cs
@@ -1,10 +1,13 @@
-using System;
 using System.Collections.Generic;
+
+using Dess.Api.Types;
 
 namespace Dess.Api.Models.ElectroFence
 {
 	public class ElectroFenceFaultDto
 	{
+		public FaultType Type { get; set; }
+
 		public long OccuredOn { get; set; }
 		public long ObviatedOn { get; set; }
 		public long ResetedOn { get; set; }
