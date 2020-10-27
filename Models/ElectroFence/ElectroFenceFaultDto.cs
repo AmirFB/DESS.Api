ccef1086e173fb1a6b012f2d0e2bc82e7f5ba1df
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-10-27 17:22:03 +0330

    merge conflicts solved.

--- Models/ElectroFence/ElectroFenceFaultDto.cs
+++ Models/ElectroFence/ElectroFenceFaultDto.cs
@@ -6,12 +6,9 @@ namespace Dess.Api.Models.ElectroFence
 {
 	public class ElectroFenceFaultDto
 	{
-<<<<<<< HEAD
 		public FaultType Type { get; set; }
 
-=======
 		public int Id { get; set; }
->>>>>>> e83f5834bfa36a1205e229763e1a1d5a95af2491
 		public long OccuredOn { get; set; }
 		public long ObviatedOn { get; set; }
 		public long ResetedOn { get; set; }
