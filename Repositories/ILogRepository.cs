064c09462113ff6ec980a6eec3868853757747b7
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-11-29 13:27:59 +0330

    "GetAsync" implemented, which gets filtered log.

--- Repositories/ILogRepository.cs
+++ Repositories/ILogRepository.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 using Dess.Api.Entities;
+using Dess.Api.Models.Filters;
 
 namespace Dess.Api.Repositories
 {
-  public interface ILogRepository : IRepositoryBase<SiteStatus>
+  public interface ILogRepository : IRepositoryBase<SiteFault>
   {
-
+    Task<IEnumerable<SiteFault>> GetAsync(ReportFilterDto filter);
   }
 }
--- Repositories/LogRepository.cs
+++ Repositories/LogRepository.cs
@@ -1,10 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
 using Dess.Api.DbContexts;
 using Dess.Api.Entities;
+using Dess.Api.Models.Filters;
+using Dess.Api.Types;
 
 namespace Dess.Api.Repositories
 {
-  public class LogRepository : RepositoryBase<SiteStatus, DessDbContext>, ILogRepository
+  public class LogRepository : RepositoryBase<SiteFault, DessDbContext>, ILogRepository
   {
     public LogRepository(DessDbContext context) : base(context) { }
+
+    public async Task<IEnumerable<SiteFault>> GetAsync(ReportFilterDto filter)
+    {
+      var from = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+      var to = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+      if (filter.From.HasValue)
+        from.AddMilliseconds((double)filter.From);
+
+      if (filter.To.HasValue)
+        from.AddMilliseconds((double)filter.To);
+
+      var logs = await Entities
+        .Include(l => l.Site)
+        .Where(l => from.Year > 2000 ? l.OccuredOn >= from : true)
+        .Where(l => to.Year > 2000 ? l.OccuredOn <= to : true)
+        .Where(l =>
+          filter.Type == ReportType.NotObviated ? l.ObviatedOn.Year <= 2000 :
+          filter.Type == ReportType.Obviated ? l.ObviatedOn.Year > 2000 :
+          filter.Type == ReportType.NotReseted ? l.ResetedOn.Year <= 2000 :
+          filter.Type == ReportType.Reseted ? l.ResetedOn.Year > 2000 :
+          true)
+        .Where(l =>
+          filter.AllGroups ? true :
+          filter.SiteGroupIds.Contains(l.Site.GroupId))
+        .Where(l =>
+          filter.AllSites ? true :
+          filter.SiteIds.Contains(l.SiteId))
+        .Where(l => filter.FaultTypes.Contains(l.Type))
+        .Where(l =>
+          filter.SeenByAll ? true :
+          filter.SeenBy.Intersect(l.SeenBy).Count() > 0)
+        .Where(l =>
+          filter.ResetedByAll ? true :
+          filter.ResetedBy.Contains(l.ResetedBy))
+        .ToListAsync();
+
+      return logs;
+    }
   }
 }
