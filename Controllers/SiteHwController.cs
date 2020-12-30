0103b470d32ab296ca4cbc4d9fe5f0077e9bb54b
Amir Fakhim Babaei <amirfakhimbabaei@gmail.com>
2020-12-30 21:09:06 +0400

    Message send using http client implemented.

--- Controllers/SiteHwController.cs
+++ Controllers/SiteHwController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Authorization;
@@ -17,6 +19,8 @@ using Dess.Api.Models.Site;
 using Dess.Api.Repositories;
 using Dess.Api.Types;
 
+using Newtonsoft.Json;
+
 namespace Dess.Api.Controllers
 {
   [AllowAnonymous]
@@ -28,8 +32,14 @@ namespace Dess.Api.Controllers
     private readonly IUserLogRepository _userLogRepository;
     private readonly IMapper _mapper;
     private readonly IHubContext<SiteHub> _hubContext;
+    private readonly IHttpClientFactory _httpClientFactory;
 
-    public SiteRepository(ISiteRepository siteRepository, IUserLogRepository userLogRepository, IMapper mapper, IHubContext<SiteHub> hubContext)
+    public SiteRepository(
+      ISiteRepository siteRepository,
+      IUserLogRepository userLogRepository,
+      IMapper mapper,
+      IHubContext<SiteHub> hubContext,
+      IHttpClientFactory httpClientFactory)
     {
       _repository = siteRepository ??
         throw new ArgumentNullException(nameof(siteRepository));
@@ -42,6 +52,9 @@ namespace Dess.Api.Controllers
 
       _hubContext = hubContext ??
         throw new ArgumentNullException(nameof(_hubContext));
+
+      _httpClientFactory = httpClientFactory ??
+        throw new ArgumentNullException(nameof(_httpClientFactory));
     }
 
     private SiteFault HandleHvFault(SiteStatus status, SiteFault fault, IEnumerable<int> users)
@@ -51,7 +64,7 @@ namespace Dess.Api.Controllers
         var output = new SiteFault { SiteId = status.SiteId };
         output.OccuredOn = DateTime.UtcNow;
         output.Type = FaultType.Hv;
-        foreach (var user in users)output.SeenBy.Add(user);
+        foreach (var user in users) output.SeenBy.Add(user);
         return output;
       }
 
@@ -68,7 +81,7 @@ namespace Dess.Api.Controllers
         var output = new SiteFault { SiteId = status.SiteId };
         output.OccuredOn = DateTime.UtcNow;
         output.Type = FaultType.Lv;
-        foreach (var user in users)output.SeenBy.Add(user);
+        foreach (var user in users) output.SeenBy.Add(user);
         return output;
       }
 
@@ -85,7 +98,7 @@ namespace Dess.Api.Controllers
         var output = new SiteFault { SiteId = status.SiteId };
         output.OccuredOn = DateTime.UtcNow;
         output.Type = FaultType.Tamper;
-        foreach (var user in users)output.SeenBy.Add(user);
+        foreach (var user in users) output.SeenBy.Add(user);
         return output;
       }
 
@@ -102,7 +115,7 @@ namespace Dess.Api.Controllers
         var output = new SiteFault { SiteId = status.SiteId };
         output.OccuredOn = DateTime.UtcNow;
         output.Type = FaultType.Power;
-        foreach (var user in users)output.SeenBy.Add(user);
+        foreach (var user in users) output.SeenBy.Add(user);
         return output;
       }
 
@@ -119,7 +132,7 @@ namespace Dess.Api.Controllers
         var output = new SiteFault { SiteId = status.SiteId };
         output.OccuredOn = DateTime.UtcNow;
         output.Type = FaultType.Input1;
-        foreach (var user in users)output.SeenBy.Add(user);
+        foreach (var user in users) output.SeenBy.Add(user);
         return output;
       }
 
@@ -136,7 +149,7 @@ namespace Dess.Api.Controllers
         var output = new SiteFault { SiteId = status.SiteId };
         output.OccuredOn = DateTime.UtcNow;
         output.Type = FaultType.Input2;
-        foreach (var user in users)output.SeenBy.Add(user);
+        foreach (var user in users) output.SeenBy.Add(user);
         return output;
       }
 
@@ -154,6 +167,32 @@ namespace Dess.Api.Controllers
       yield return HandlePowerFault(status, faults.FirstOrDefault(f => f.Type == FaultType.Power), users);
       yield return HandleInput1Fault(status, faults.FirstOrDefault(f => f.Type == FaultType.Input1), users);
       yield return HandleInput2Fault(status, faults.FirstOrDefault(f => f.Type == FaultType.Input2), users);
+    }
+
+    private async Task SendMessage(
+      Site site,
+      IEnumerable<SiteFault> raised,
+      IEnumerable<SiteFault> obviated)
+    {
+      var text = FaultString.GetFaultMessage(site, raised, obviated);
+      var numbers = _repository.GetGroupPhoneNumbersAsync(site.GroupId);
+
+      var httpClient = _httpClientFactory.CreateClient();
+
+      var request = new HttpRequestMessage(HttpMethod.Get,
+        "http://api.payamak-panel.com/post/Send.asmx");
+
+      var body = new
+      {
+        Username = "",
+        Password = "",
+        From = "",
+        To = numbers,
+        Text = text,
+        Isflash = false
+      };
+      var json = JsonConvert.SerializeObject(body);
+      var content = new StringContent(json, Encoding.UTF8, "application/json");
     }
 
     [HttpPost("{siteId}/{configHash}")]
@@ -176,15 +215,38 @@ namespace Dess.Api.Controllers
       site.Applied = configHash == site.Hash;
 
       var statusHash = (status as IHashable).GetHash();
+      var obviated = new List<SiteFault>();
+      obviated.AddRange(site.NotObviatedFaults);
+      var raised = new List<SiteFault>();
 
       foreach (var fault in HandleFaults(site.Status, site.NotObviatedFaults, SiteHub.UserIds))
+      {
         if (fault != null)
+        {
           site.Log.Add(fault);
+
+          raised.Add(fault);
+        }
+      }
+
+      var notObviated = new List<SiteFault>();
+      notObviated.AddRange(site.NotObviatedFaults);
+
+      foreach (var fault in notObviated)
+        obviated.Remove(fault);
+
+      if (raised.Count() > 0 || obviated.Count() > 0)
+        await SendMessage(site, raised, obviated);
 
       if (site.AutoLocation)
       {
-        site.Latitude = status.Latitude;
-        site.Longitude = status.Longitude;
+        float dummy;
+
+        if (float.TryParse(status.Latitude, out dummy))
+          site.Latitude = status.Latitude;
+
+        if (float.TryParse(status.Longitude, out dummy))
+          site.Longitude = status.Longitude;
       }
 
       await _repository.SaveAsync();
